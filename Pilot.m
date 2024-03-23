0bf0e442be81740ab9d20f44d08c361c538f8252
Vũ Vương Quốc Anh <quanhuk0903@gmail.com>
2024-03-23 15:47:58 +0100

    Add cognitive radio code

--- Pilot.m
+++ Pilot.m
@@ -12,17 +12,16 @@ message = "Hello World!" ;
 Fs = 10e6 ;
 Fc = 921.5e6 ;
 %% Transmit Signal
-signalIQ = LoRa_Tx(message,BW,SF,Power,Fs,Fc - fc) ;
+signalIQ = LoRa_Tx(message,BW,SF,Power,Fs,Fc-fc) ;
 Sxx = 10*log10(rms(signalIQ).^2) ;
 disp(['Transmit Power   = ' num2str(Sxx) ' dBm'])
 %% Plots
 figure(1)
 spectrogram(signalIQ,500,0,500,Fs,'yaxis','centered')
-figure(2)
-obw(signalIQ,Fs) ;
+%figure(2)
+%obw(signalIQ,Fs) ;
 %% Received Signal
-message_out = LoRa_Rx(signalIQ,BW,SF,2,Fs,Fc - fc) ;
+message_out = LoRa_Rx(signalIQ,BW,SF,2,Fs,Fc-fc) ;
 %% Message Out
-disp('Message Received =')
-disp(message_out)
+disp(['Message Received = ' char(message_out)])
 
